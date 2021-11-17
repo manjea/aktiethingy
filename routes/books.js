6f249b97e34270338520e14c52258f80effb903a
codecat111 <codecat111@gmail.com>
2021-11-17 07:28:43 +0100

    Books creater/index/new

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs')
 const Book = require('../models/book')
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author')
@@ -15,7 +16,26 @@ const upload = multer({
 
 //All Books route
 router.get('/', async (req, res) => {
-    res.send('All books')
+    let query = Book.find()
+    if (req.query.title != null && req.query.title != ''){
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if (req.query.publishedBefore != null && req.query.publishedBefore != ''){
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if (req.query.publishedAfter != null && req.query.publishedAfter != ''){
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    try{
+        const books = await query.exec()
+        res.render('books/index', {
+            books: books,
+            searchOptions: req.query
+        })
+    }catch{
+        res.redirect('/')
+    }
+    
 })
 
 //New Books route
@@ -34,16 +54,24 @@ router.post('/', upload.single('cover'), async (req, res) => {
         coverImageName: fileName,
         description: req.body.description
     })
-    console.log(fileName)
+
     try {
         const newBook = await book.save()
         //res.redirect(`books/${newBook.id}`)
         res.redirect(`books`)
     } catch{
-        console.log('wring')
+        if(book.coverImageName != null){
+            removeBookCover(book.coverImageName)
+        }
         renderNewPage(res, book, true)
     }
 })
+
+function removeBookCover(fileName){
+    fs.unlink(path.join(uploadPath, fileName), err => {
+        if(err) console.error(err)
+    })
+}
 
 async function renderNewPage(res, book, hasError = false){
     try {
